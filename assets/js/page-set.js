188c4741fc1ca75cc919f088c5fe829995e62a93
h5avlov <h5avlov@abv.bg>
2020-09-29 21:16:44 +0300

    Main navigation functionality to switch a class over a list of page sections added

--- assets/js/page-set.js
+++ assets/js/page-set.js
@@ -1,4 +1,23 @@
 ( function() { 
-	$( '.page-set' ).bxSlider();  
+	// Getting references to all page sections
+	const pages = $( '.page-set .section' ); 
+
+	// Referencing the first page section as current 
+	let current = pages
+		.filter( '.current' ); 
+
+	$( '.page-set' ).on( 'click', '.nav-button', function( e ) { 
+		// Getting button target page position 
+		const pos = $( this )
+			.attr( 'data-pos' ); 
+
+		// Moving current class name from former to latter page section 
+		current
+			.removeClass( 'current' ); 
+		current = pages
+			.eq( pos )
+			.addClass( 'current' ); 
+
+	} ); 
 
 }() )
