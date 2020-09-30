833749bfaf1625a3d43f2dccd053d2e9d2db2abf
h5avlov <h5avlov@abv.bg>
2020-09-30 09:00:28 +0300

    Vegas slideshow plugin applied to body

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,6 +1,21 @@
 ( function() { 
-	$( '.gallery .random-access' )
-		.prependTo( '.page' ); 
-	
+	$( "body" ).vegas( {
+		slides: [
+		    { src: "/assets/images/74273925_2574521065964230_343086875277262848_n.jpg" }, 
+		    { src: "/assets/images/95629555_2959433724139627_3982936818139004928_n.jpg" }, 
+		    { src: "/assets/images/95409482_2959433690806297_4039085930621960192_n.jpg" }, 
+		    { src: "/assets/images/70023155_2448442838572054_2379843418823065600_n.jpg" }, 
+		    { src: "/assets/images/69617793_2448442805238724_1076493570891841536_n.jpg" },
+		    { src: "/assets/images/10981862_766400356776319_2442975887485609845_n.jpg" },
+		    { src: "/assets/images/10982484_766400493442972_7814646519377612615_n.jpg" },
+		    { src: "/assets/images/10984064_766400473442974_5778066791930443440_n.jpg" },
+		    { src: "/assets/images/10426125_766400340109654_7639500533959033844_n.jpg" }, 
+		    { src: "/assets/images/10982484_766400493442972_7814646519377612615_nn.jpg" },
+		], 
 
+		overlay: '/assets/plugins/vegas/overlays/01.png', 
+
+		transitionDuration: 2000, 
+		transition: 'zoomOut'
+	 } ); 
 }() )
