30eb9d35cdc81d0ce1c29f62fd1b2d02bb3dbf6d
h5avlov <h5avlov@abv.bg>
2021-01-16 16:58:18 +0200

    Website turned back to its first version with a slideshow plugin in the background and a header on top

--- assets/js/main-nav.js
+++ assets/js/main-nav.js
@@ -1,18 +1,18 @@
-( function() {  
-	$( '.main-nav' )
-		.prependTo( '.page-set .text-content' ) 
-		.on( 'click', function( e ) {  
-			const target = $( e.target ); 
-			if ( !target.is( '.nav-button' ) ) { 
-				return; 
-			} 
+// ( function() {  
+// 	$( '.main-nav' )
+// 		.prependTo( '.page-set .text-content' ) 
+// 		.on( 'click', function( e ) {  
+// 			const target = $( e.target ); 
+// 			if ( !target.is( '.nav-button' ) ) { 
+// 				return; 
+// 			} 
 
-			// const targetSectionClassName = target.attr( 'data-target-section' ); 
-			// const targetSection = $( '.' + targetSectionClassName ); 
-			// sectionControl.openSection( targetSection ); 
+// 			// const targetSectionClassName = target.attr( 'data-target-section' ); 
+// 			// const targetSection = $( '.' + targetSectionClassName ); 
+// 			// sectionControl.openSection( targetSection ); 
 
-			sectionControl.openSection( target ); 
+// 			sectionControl.openSection( target ); 
 			
-		} ); 
+// 		} ); 
 
-}() ) 
+// }() ) 
--- assets/js/page-set.js
+++ assets/js/page-set.js
@@ -1,33 +1,33 @@
-( function() { 
-	let galleryOpen = false; 
+// ( function() { 
+// 	let galleryOpen = false; 
 
-	$( '.gallery' ).on( 'click', function( e ) { 
-		const target = $( e.target ); 
-		const switchGalleryButton = $( '.switch-gallery-button' ); 
+// 	$( '.gallery' ).on( 'click', function( e ) { 
+// 		const target = $( e.target ); 
+// 		const switchGalleryButton = $( '.switch-gallery-button' ); 
 
-		if ( !target.is( switchGalleryButton ) ) { 
-			return; 
-		} 
+// 		if ( !target.is( switchGalleryButton ) ) { 
+// 			return; 
+// 		} 
 
-		if ( !galleryOpen ) { 
-			sectionControl.openGallery();  
-		} else { 
-			sectionControl.closeGallery(); 
-		} 
+// 		if ( !galleryOpen ) { 
+// 			sectionControl.openGallery();  
+// 		} else { 
+// 			sectionControl.closeGallery(); 
+// 		} 
 
-		galleryOpen = !galleryOpen; 
+// 		galleryOpen = !galleryOpen; 
 
-	} ); 
+// 	} ); 
 
-	// $( '.page-set' ).lightSlider( { 
-	// 	// controls: false 
-	// 	// mode: 'vertical' 
-	// 	vertical: true
+// 	// $( '.page-set' ).lightSlider( { 
+// 	// 	// controls: false 
+// 	// 	// mode: 'vertical' 
+// 	// 	vertical: true
 
-	// } ); 
-
-	
+// 	// } ); 
 
 	
+
 	
-}() )
+	
+// }() )
