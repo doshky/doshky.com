76d0168b17dd72ceb1a9daac7397b630d1b2197c
h5avlov <h5avlov@abv.bg>
2021-07-23 13:25:33 +0300

    Animation end transition, revealing website front page, moved from CSS to jQuery

--- assets/js/smooth-scroll.js
+++ assets/js/smooth-scroll.js
@@ -7,12 +7,28 @@ function smoothScrollHandler( e ) {
 	// Getting the button target section
 	const targetSectionSelector = $( this ).attr( 'data-target-section' ); 
 
+	$( '.section-current' ).removeClass( 'section-current' ); 
+
 	// Scrolling to the button target section 
 	const scrollDistance = $( targetSectionSelector ).offset().top; 
-	$( 'html, body' ).animate( { scrollTop: scrollDistance }, 500 ); 
- 
-	// Updates previous and current section 
-	$( '.section-current' ).removeClass( 'section-current' ); 
-	$( targetSectionSelector ).addClass( 'section-current' ); 
+	$( 'html, body' ).animate( { scrollTop: scrollDistance }, 500, function() { 
+		// Updates previous and current section  
+		$( targetSectionSelector ).addClass( 'section-current' );
 
+	} );  
+
+} 
+
+setTimeout( animationScrollHandler, 5000 ); 
+function animationScrollHandler( e ) { 
+	// Getting the button target section
+	const targetSectionSelector = '#welcome-section'; 
+
+	// Scrolling to the button target section 
+	const scrollDistance = $( targetSectionSelector ).offset().top; 
+	$( 'html, body' ).animate( { scrollTop: scrollDistance }, 500, function() { 
+		$( targetSectionSelector ).addClass( 'section-current' ); 
+
+	} ); 
+ 
 } 
