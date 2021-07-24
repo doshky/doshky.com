568dd958bcfa42c34312a32c43d4c077c315647b
h5avlov <h5avlov@abv.bg>
2021-07-24 13:21:37 +0300

    Smooth scroll handler updated to control intro animation play in addition to button click and section transition

--- assets/js/smooth-scroll.js
+++ assets/js/smooth-scroll.js
@@ -1,34 +1,55 @@
+// Scroll handler is called on page load to transition to welcome section on animation end 
+smoothScrollHandler();  
+
+// Scroll handler is called on button click 
 $( document ).on( 'click', '.inner-link', smoothScrollHandler ); 
 
 function smoothScrollHandler( e ) { 
-	// Preventing default click response
-	e.preventDefault(); 
-
-	// Getting the button target section
-	const targetSectionSelector = $( this ).attr( 'data-target-section' ); 
-
-	$( '.section-current' ).removeClass( 'section-current' ); 
-
-	// Scrolling to the button target section 
-	const scrollDistance = $( targetSectionSelector ).offset().top; 
-	$( 'html, body' ).animate( { scrollTop: scrollDistance }, 500, function() { 
-		// Updates previous and current section  
-		$( targetSectionSelector ).addClass( 'section-current' );
-
-	} );  
-
-} 
-
-setTimeout( animationScrollHandler, 5000 ); 
-function animationScrollHandler( e ) { 
-	// Getting the button target section
-	const targetSectionSelector = '#welcome-section'; 
-
-	// Scrolling to the button target section 
-	const scrollDistance = $( targetSectionSelector ).offset().top; 
-	$( 'html, body' ).animate( { scrollTop: scrollDistance }, 500, function() { 
-		$( targetSectionSelector ).addClass( 'section-current' ); 
-
-	} ); 
+	// Button target section selector
+	let targetSectionSelector;  
+	// Button target section 
+	let targetSection; 
+	// Default scroll delay, used when page section is the target  
+	let scrollDelay = 0; 
+	// Default scroll duration 
+	const scrollDuration = 500; 
  
+	// Scroll handler is called not only on button click, 
+	// but on page load and after scrolling to intro animation section too 
+	if ( e ) { 
+		// Preventing default click response
+		e.preventDefault(); 
+
+		// Getting the button target section
+		targetSectionSelector = $( this ).attr( 'data-target-section' );    
+	} 
+
+	// On call after scrolling to intro animation section 
+	if ( $( '#intro-animation' ).hasClass( 'section-current' ) ) { 
+		// Sets moving to welcome section
+		targetSectionSelector = '#welcome-section'; 
+		// Ensures that animation is over before moving to welcome section 
+		scrollDelay = 5000; 
+
+	} 
+
+	// Getting target section 
+	targetSection = $( targetSectionSelector ); 
+	// Scrolling distance to the button target section 
+	const scrollDistance = targetSection.offset().top; 
+
+	setTimeout( function() { 
+		$( 'html, body' ).animate( { scrollTop: scrollDistance }, scrollDuration, function() { 
+			// Updates previous and current section  
+			$( '.section-current' ).removeClass( 'section-current' ); 
+			targetSection.addClass( 'section-current' ); 
+
+			// Moving to intro animation section calls scroll handler to move to welcome page after waiting animation to end
+			if ( $( '#intro-animation' ).hasClass( 'section-current' ) ) { 
+				smoothScrollHandler(); 
+			}
+
+		} ); 
+	}, scrollDelay ); 
+
 } 
