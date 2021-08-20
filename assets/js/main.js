d2a556e489508fc3eb840b5b770a65ea21157162
h5avlov <h5avlov@abv.bg>
2021-08-20 18:24:49 +0300

    Reverted back to section transition, based on ection index and section height

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,4 @@
-( function() { 
-    // Moves current section marking class name from prev to next section
-    const moveCurrentClassName = ( function( currentClassName ) { 
-        return function( currentSectionSelector ) { 
-            $( '.' + currentClassName ).removeClass( currentClassName ); 
-            $( currentSectionSelector ).addClass( currentClassName ); 
-        }
-
-    }( 'section-current' ) );
-
-    // Scrolls page to target section and marks it as current section 
-    const scrollToSection = ( function( duration, callback ) { 
-        return function( selector ) { 
-            const scrollTop = $( selector ).offset().top; 
-
-            $( 'html' ).animate( { 
-                scrollTop: scrollTop 
-            }, duration, function() { 
-                callback( selector ); 
-            } ); 
-        }
-
-    }( 500, moveCurrentClassName ) ); 
-
-    // Starts intro animation and scrolls to welcome section on animation end 
-    const playIntro = ( function( animationDuration ) { 
-        return function() { 
-            moveCurrentClassName( '#intro-animation' ); 
-
-            const timeout = setTimeout( function() { 
-                scrollToSection( '#welcome-section' ); 
-            }, animationDuration ); 
-        }
-    }( 3500 ) ); 
-
-    playIntro(); 
-
-    $( document ).on( 'click', '.inner-link', function( e ) { 
-        const innerLink = $( e.target ); 
-      
-        e.preventDefault(); 
-
-        const targetSectionSelector = innerLink.attr( 'data-target-section' ); 
-
-        scrollToSection( targetSectionSelector ); 
-
-        if ( innerLink.attr( 'data-target-section' ) === '#intro-animation' ) { 
-            playIntro(); 
-        }
-
-    } ); 
-}() ); 
-
-
 // ( function() { 
-//     const sectionHeight = $( '#intro-animation' ).height(); 
-
 //     // Moves current section marking class name from prev to next section
 //     const moveCurrentClassName = ( function( currentClassName ) { 
 //         return function( currentSectionSelector ) { 
@@ -62,19 +6,21 @@
 //             $( currentSectionSelector ).addClass( currentClassName ); 
 //         }
 
-//     }( 'section-current' ) ); 
+//     }( 'section-current' ) );
 
 //     // Scrolls page to target section and marks it as current section 
 //     const scrollToSection = ( function( duration, callback ) { 
-//         return function( sectionPos ) { 
-//             const scrollDistance = -1 * ( parseInt( sectionPos ) * sectionHeight );  
+//         return function( selector ) { 
+//             const scrollTop = $( selector ).offset().top; 
 
-//             $( '.scroll-track' ).animate( { 
-//                 top: scrollDistance + 'px' 
-//             }, duration ); 
+//             $( '.page' ).animate( { 
+//                 scrollTop: scrollTop 
+//             }, duration, function() { 
+//                 callback( selector ); 
+//             } ); 
 //         }
 
-//     }( 500 ) ); 
+//     }( 500, moveCurrentClassName ) ); 
 
 //     // Starts intro animation and scrolls to welcome section on animation end 
 //     const playIntro = ( function( animationDuration ) { 
@@ -82,7 +28,7 @@
 //             moveCurrentClassName( '#intro-animation' ); 
 
 //             const timeout = setTimeout( function() { 
-//                 scrollToSection( 1 ); 
+//                 scrollToSection( '#welcome-section' ); 
 //             }, animationDuration ); 
 //         }
 //     }( 3500 ) ); 
@@ -90,17 +36,67 @@
 //     playIntro(); 
 
 //     $( document ).on( 'click', '.inner-link', function( e ) { 
-//         const innerLink = $( e.target ); 
-      
 //         e.preventDefault(); 
 
-//         const sectionPos = innerLink.attr( 'data-section-pos' ); 
+//         const innerLink = $( e.target );  
+//         const targetSectionSelector = innerLink.attr( 'data-target-section' ); 
 
-//         scrollToSection( sectionPos ); 
+//         scrollToSection( targetSectionSelector ); 
 
-//         if ( innerLink.attr( 'data-target-section' ) === '#intro-animation' ) { 
-//             playIntro(); 
-//         }
+//         // if ( innerLink.attr( 'data-target-section' ) === '#intro-animation' ) { 
+//         //     playIntro(); 
+//         // }
 
 //     } ); 
 // }() ); 
+
+
+( function() {  
+    // Moves current section marking class name from prev to next section
+    const moveCurrentClassName = ( function( currentClassName ) { 
+        return function( currentSectionSelector ) { 
+            $( '.' + currentClassName ).removeClass( currentClassName ); 
+            $( currentSectionSelector ).addClass( currentClassName ); 
+        }
+
+    }( 'section-current' ) ); 
+
+    // Scrolls page to target section and marks it as current section 
+    const scrollToSection = ( function( duration, callback ) {  
+        const sectionHeight = $( '#intro-animation' ).height(); 
+        return function( sectionPos ) {  
+            const scrollDistance = -1 * ( parseInt( sectionPos ) * sectionHeight );  
+
+            $( '.scroll-track' ).animate( { 
+                top: scrollDistance + 'px' 
+            }, duration ); 
+        }
+
+    }( 500 ) ); 
+
+    // Starts intro animation and scrolls to welcome section on animation end 
+    const playIntro = ( function( animationDuration ) { 
+        return function() { 
+            moveCurrentClassName( '#intro-animation' ); 
+
+            const timeout = setTimeout( function() { 
+                scrollToSection( 1 ); 
+            }, animationDuration ); 
+        }
+    }( 3500 ) ); 
+
+    playIntro(); 
+
+    $( document ).on( 'click', '.inner-link', function( e ) { 
+        e.preventDefault(); 
+
+        const innerLink = $( e.target );  
+        const sectionPos = innerLink.attr( 'data-section-pos' ); 
+        scrollToSection( sectionPos ); 
+
+        if ( innerLink.attr( 'data-target-section' ) === '#intro-animation' ) { 
+            playIntro(); 
+        }
+
+    } ); 
+}() ); 
