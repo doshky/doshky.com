544783fc90087ea20574d2a55d998fe59810b70b
h5avlov <h5avlov@abv.bg>
2021-08-21 15:29:21 +0300

    Page transition turned off, currently no way to track current section on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,38 +9,42 @@
     }( 'section-current' ) );
 
     // Scrolls page to target section and marks it as current section 
-    const scrollToSection = ( function( duration, callback ) { 
-        return function( selector ) { 
-            const scrollTop = $( selector ).offset().top; 
+    // const scrollToSection = ( function( duration, callback ) { 
+    //     return function( selector ) { 
+    //         const scrollTop = $( selector ).offset().top; 
 
-            $( 'html, body' ).animate( { 
-                scrollTop: scrollTop 
-            }, duration, function() { 
-                callback( selector ); 
-            } ); 
-        }
+    //         $( 'html, body' ).animate( { 
+    //             scrollTop: scrollTop 
+    //         }, duration, function() { 
+    //             callback( selector ); 
+    //         } ); 
+    //     }
 
-    }( 500, moveCurrentClassName ) ); 
+    // }( 500, moveCurrentClassName ) ); 
 
     // Starts intro animation and scrolls to welcome section on animation end 
-    const playIntro = ( function( animationDuration ) { 
-        return function() { 
-            moveCurrentClassName( '#intro-animation' ); 
+    // const playIntro = ( function( animationDuration ) { 
+    //     return function() { 
+    //         moveCurrentClassName( '#intro-animation' ); 
 
-            const timeout = setTimeout( function() { 
-                scrollToSection( '#welcome-section' ); 
-            }, animationDuration ); 
-        }
-    }( 3500 ) ); 
+    //         const timeout = setTimeout( function() { 
+    //             scrollToSection( '#welcome-section' ); 
+    //         }, animationDuration ); 
+    //     }
+    // }( 3500 ) ); 
 
-    playIntro(); 
+    // playIntro(); 
+
+    moveCurrentClassName( '#welcome-section' ); 
 
     $( document ).on( 'click', '.inner-link', function( e ) { 
-        e.preventDefault(); 
+        // e.preventDefault(); 
 
         const innerLink = $( e.target );  
         const targetSectionSelector = innerLink.attr( 'data-target-section' ); 
-        scrollToSection( targetSectionSelector ); 
+        // scrollToSection( targetSectionSelector ); 
+
+        moveCurrentClassName( targetSectionSelector ); 
 
         // if ( innerLink.attr( 'data-target-section' ) === '#intro-animation' ) { 
         //     playIntro(); 
