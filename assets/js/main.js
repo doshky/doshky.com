7daff9f5268adaef275c1b7a2d5e99340343ba6b
h5avlov <h5avlov@abv.bg>
2022-03-12 15:46:03 +0200

    Section opening animation updated with banner moving to reveal navigation on navigation open button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,91 +1,3 @@
-//( function() { 
-//    window.addEventListener( 'click', function( e ) { 
-//        const clickTarget = e.target; 
-//        if ( clickTarget.classList.contains( 'control-button' ) ) { 
-//            if ( !clickTarget.classList.contains( 'button-open-page' ) ) { 
-//                e.preventDefault(); 
-//            }
-            
-//            const expandTargetID = clickTarget.getAttribute( 'href' );  
-//            const expandTarget = document.querySelector( expandTargetID ); 
-
-//            if ( clickTarget.classList.contains( 'button-open' ) ) { 
-//                expandTarget.classList.add( 'section-open' ); 
-//            } else if ( clickTarget.classList.contains( 'button-close' ) ) { 
-//                expandTarget.classList.remove( 'section-open' ); 
-
-//                if ( clickTarget.classList.contains( 'button-close-section' ) ) { 
-//                    expandTarget.classList.remove( 'page-section-open' ); 
-
-//                    const section = clickTarget.closest( '.section' ); 
-//                    section.classList.remove( 'section-open' ); 
-
-//                }
-//            } 
-
-//            if ( clickTarget.classList.contains( 'nav-link' ) ) { 
-//                const pageBody = document.querySelector( '#page-body' ); 
-//                pageBody.classList.add( 'page-section-open' ); 
-//            }
-
-//        } 
-//    } ); 
-
-//}() ); 
-
-//( function() { 
-//    window.addEventListener( 'click', function( e ) { 
-//        const clickTarget = e.target; 
-//        const hasClass = hasClassElement( clickTarget ); 
-
-//        if ( !hasClass( 'control-button' ) ) { 
-//            return; 
-//        } 
-
-//        // Control class name to change the state of an element 
-//        const controlClassName = clickTarget.getAttribute( 'data-control-class-name' ); 
-
-//        // Target element to change state 
-//        const controlTargetSelector = clickTarget.getAttribute( 'href' ); 
-//        const controlTarget = document.querySelector( controlTargetSelector ); 
-
-//        // The control class name is added to or removed from the target element based on button type 
-//        if ( hasClass( 'control-button-on' ) ) { 
-//            controlTarget.classList.add( controlClassName ); 
-//        } else if ( hasClass( 'control-button-off' ) ) { 
-//            controlTarget.classList.remove( controlClassName ); 
-//        } 
-
-//        if ( hasClass( 'nav-link' ) ) { 
-//            document
-//                .querySelector( '#page-body' )
-//                .classList
-//                .add( 'content-section-on' ); 
-
-//        } 
-
-//        if ( hasClass( 'button-close-section' ) ) { 
-//            document
-//                .querySelector( '#page-body' )
-//                .classList
-//                .remove( 'content-section-on' ); 
-
-//        }
-       
-//    } ); 
-
-//    function hasClassElement( element ) { 
-//        const classList = element.classList; 
-
-//        return function( className ) { 
-//            return classList.contains( className ); 
-//        }
-
-//    }
-
-//}() ); 
-
-
 ( function() { 
     window.addEventListener( 'click', function( e ) { 
         const clickTarget = e.target; 
@@ -95,7 +7,7 @@
             return; 
         } 
 
-        if ( !hasClass( 'button-open-page' ) ) {
+        if ( !hasClass( 'button-open-page' ) && !hasClass( 'details-button' ) ) {
             e.preventDefault(); 
         }
 
