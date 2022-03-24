e80f634122d56a1cf07f089f03e5f623a39a7f72
h5avlov <h5avlov@abv.bg>
2022-03-24 16:13:12 +0200

    Gallery page functionality set up

---
+++ assets/js/gallery.js
@@ -0,0 +1,9 @@
+( function() { 
+    $( '#work-section .work-section-entry-list' ).magnificPopup( { 
+        delegate: 'a', 
+        type: 'image', 
+        gallery: {
+            enabled: true 
+        } 
+    } ); 
+}() ); 
