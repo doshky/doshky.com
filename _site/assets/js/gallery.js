64bc2068f16febf49a287f264d58b1f29ddc6937
h5avlov <h5avlov@abv.bg>
2022-12-01 11:00:36 +0200

    Dotted line moved from 'owner' to footer

--- _site/assets/js/gallery.js
+++
@@ -1,9 +0,0 @@
-( function() { 
-    $( '#work-section .work-section-entry-list' ).magnificPopup( { 
-        delegate: 'a', 
-        type: 'image', 
-        gallery: {
-            enabled: true 
-        } 
-    } ); 
-}() ); 
